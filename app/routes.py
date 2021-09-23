cc4d00de4dcabc3500ff49f3bcc5596a49aa71c6
Vikash Yadav <vikash.yadav@scientisttechnologies.com>
2021-09-23 23:05:04 +0530

    Templates added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Welcome to the Scientist Technologies!"
+    user = {'username': 'Vicky'}
+    posts = [
+            {
+                'author': {'username': 'Abhi'},
+                'body': 'Beautiful day in Bangalore!'
+                },
+            {
+                'author': {'username': 'Kiran'},
+                'body': 'The Avengers movie was so cool!'
+                },
+            ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+#    return "Welcome to the Scientist Technologies!"
